88eb0f9477fa3d2a759141ede2e454209a002459
appyspaces <appyspaces@gmail.com>
2024-08-25 15:05:11 +0300

    feat: :zap: use products library in inventory application

--- apps/inventory/src/app/app.component.ts
+++ apps/inventory/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductsComponent } from '@angular-monorepo-tutorial/products';
+// TODO:(td) if we remove this we should remove other files also (technical depth)
+// import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ProductsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
