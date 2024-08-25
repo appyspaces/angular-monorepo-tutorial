4f86d77c0cc3c815ba1e9376f82a9b1d4fe237e0
appyspaces <appyspaces@gmail.com>
2024-08-25 14:50:44 +0300

    feat: :sparkles: add products and orders libraries to angular-store application
    configure products and orders routes in angular-store application

--- apps/angular-store/src/app/app.routes.ts
+++ apps/angular-store/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadComponent: () => 
+      import('@angular-monorepo-tutorial/products').then(m => m.ProductsComponent),
+  },
+  {
+    path: 'orders',
+    loadComponent: () => 
+      import('@angular-monorepo-tutorial/orders').then(m => m.OrdersComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  }
+  
+];
